31f57020f1fb25fed72da9903c37a44ae800650f
demo <manoharan12105@gmail.com>
2026-08-21 10:30:40 +0530

    first Commit

---
+++ src/main/java/me/mano/SpringBootECommerce/controller/CategoryController.java
@@ -0,0 +1,19 @@
+package me.mano.SpringBootECommerce.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import me.mano.SpringBootECommerce.model.Category;
+
+@RestController
+public class CategoryController {
+  
+  private List<Category> categories = new ArrayList<>();
+
+  @GetMapping("api/public/categories")
+  public List<Category> getAllCategories() {
+    return categories;
+  }
+}
---
+++ src/main/java/me/mano/SpringBootECommerce/model/Category.java
@@ -0,0 +1,11 @@
+package me.mano.SpringBootECommerce.model;
+
+import lombok.Data;
+
+@Data
+public class Category {
+  private Long categoryId;
+  private String categoryName;
+
+  
+}
