118067a94f8b4d602c743352ac5fa00b2a33a661
demo <manoharan12105@gmail.com>
2026-08-29 21:47:06 +0530

    passwordEncoder

--- src/main/java/me/mano/SpringBootECommerce/config/SecurityConfig.java
+++ src/main/java/me/mano/SpringBootECommerce/config/SecurityConfig.java
@@ -10,6 +10,8 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
@@ -47,16 +49,16 @@ public class SecurityConfig {
     if(!userDetailsManager.userExists("user1")) {
       userDetailsManager.createUser(
         User.withUsername("user1")
-            .password("{noop}password1")
+            .password(passwordEncoder().encode("password1"))
             .roles("USER")
             .build()
-      );
+      );   
     }
 
     if(!userDetailsManager.userExists("admin")) {
       userDetailsManager.createUser(
         User.withUsername("admin")
-            .password("{noop}adminPass")
+            .password(passwordEncoder().encode("adminPass"))
             .roles("ADMIN")
             .build()
       );
@@ -64,6 +66,11 @@ public class SecurityConfig {
 
     return userDetailsManager;
     // return new InMemoryUserDetailsManager(user1, admin);
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder(12);
   }
   
 }
